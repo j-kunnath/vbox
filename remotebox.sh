91e6bf6e34184c8c8f4442e39f0cacecfb0a9216
j-kunnath <134174125+j-kunnath@users.noreply.github.com>
2024-10-27 08:28:47 +1100

    Create remotebox.sh

---
+++ remotebox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Install dependencies
+apt-get install libgtk3-perl libsoap-lite-perl freerdp2-x11 tigervnc-viewer
+
+# Get latest remotebox
+wget https://remotebox.knobgoblin.org.uk/downloads/RemoteBox-3.3.tar.bz2
+
+# Extract TAR files
+tar -xvf RemoteBox-3.3.tar.bz2 -C /home/$USER/RemoteBox
+cd RemoteBox
